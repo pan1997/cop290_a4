4f17d01f069fa8856cf713bed914a83d770fd974
Pankaj Kumar <pankajkumaryadav1997@gmail.com>
2016-04-12 22:01:06 +0530

    simple physics added

--- src/cop290/a4/Main.java
+++ src/cop290/a4/Main.java
@@ -15,12 +15,30 @@ public class Main {
         } catch (Exception e) {
         }
         JFrame main = new JFrame("Game");
+        //animPanelTest animPanel = new animPanelTest(500, 500, 100, 1);
+        animPanel animPanel = new board(500, 500, 100, 1);
+        main.setContentPane(animPanel);
+        //animPanel.addMouseMotionListener(animPanel);
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main.setSize(600, 700);
+        main.setVisible(true);
         {
             JFrame terminal = new JFrame("terminal");
             JTextArea jt = new JTextArea();
             JTextField jtf = new JTextField();
             jtf.addActionListener(e -> {
                 jt.append(e.getActionCommand() + "\n");
+                switch (e.getActionCommand()){
+                    case "exit":
+                        System.exit(0);
+                        break;
+                    case "start":
+                        animPanel.start();
+                        break;
+                    case "stop":
+                        animPanel.stop();
+                        break;
+                }
                 if (e.getActionCommand().equalsIgnoreCase("exit"))
                     System.exit(0);
                 jtf.setText("");
@@ -31,22 +49,5 @@ public class Main {
             terminal.setSize(600, 200);
             terminal.setVisible(true);
         }
-        //animPanelTest animPanel = new animPanelTest(500, 500, 100, 1);
-        animPanel animPanel = new board(500, 500, 100, 1);
-        main.setContentPane(animPanel);
-        //animPanel.addMouseMotionListener(animPanel);
-        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        main.setSize(600, 700);
-        main.setVisible(true);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-                }
-                animPanel.start();
-            }
-        }).start();
     }
 }
